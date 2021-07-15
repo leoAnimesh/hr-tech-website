4ecbe193d738f58693c7d350d3f537da6e12aab9
Animesh Mondal <77456003+leoAnimesh@users.noreply.github.com>
2021-07-16 01:37:45 +0530

    final commit

--- Js/script.js
+++
@@ -1,7 +0,0 @@
-const togglebtn = document.getElementById('toggle-btn')
-const navbar = document.getElementsByTagName('nav')[0]
-
-togglebtn.addEventListener('click',()=>{
-    navbar.style.transition = " 1s all ease"
-    navbar.classList.toggle('toggle')
-})
